479e375857dc3d8abc0fa057cc11287ce3b79fd7
Kate <kataroslakova10@gmail.com>
2023-01-30 15:05:52 +0300

    finalize

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+
+int maximal = b;
+
+Console.Write("Максимальное число: ");
+Console.Write(maximal);
+
+
