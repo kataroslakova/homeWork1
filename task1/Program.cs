3a511928db70fb7cca6ba92a997a6f38ce366645
Kate <kataroslakova10@gmail.com>
2023-01-30 14:13:40 +0300

    task1: finding maximum of 2

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Ведите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int maximal = 0;
+
+if (a > b)
+{
+    maximal = a;
+    
+    Console.Write("Максимальное число: ");
+    Console.Write(maximal);
+}
+else
+{
+    maximal = b;
+
+    Console.Write("Максимальное число: ");
+    Console.Write(maximal);
+}
